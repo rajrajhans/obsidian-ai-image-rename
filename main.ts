7de9c04c27dac12df12891b72e326a54c5debba8
Raj Rajhans <me@rajrajhans.com>
2024-07-28 16:30:32 +0530

    chore: cp from obsidian-sample-plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,333 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	MarkdownView,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TAbstractFile,
+	TFile,
+} from "obsidian";
+import * as path from "path";
+import OpenAI from "openai";
 
-// Remember to rename these classes and interfaces!
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+	dangerouslyAllowBrowser: true,
+});
 
-interface MyPluginSettings {
+const PASTED_IMAGE_PREFIX = "Pasted image ";
+
+interface ImageRenamePluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: ImageRenamePluginSettings = {
+	mySetting: "default",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ImageRenamePlugin extends Plugin {
+	settings: ImageRenamePluginSettings;
 
 	async onload() {
 		await this.loadSettings();
+		this.addSettingTab(new ImageRenamePluginSettingTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerEvent(
+			this.app.vault.on("create", (file) => {
+				if (!(file instanceof TFile)) return;
+				const timeGapMs = new Date().getTime() - file.stat.ctime;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+				// if the file is created more than 1 second ago, the event is most likely be fired on vault initialization when starting Obsidian app, ignore it
+				if (timeGapMs > 1000) return;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+				// always ignore markdown file creation
+				if (isMarkdownFile(file)) return;
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if (isPastedImage(file)) {
+					this.renameFile(file);
 				}
+			})
+		);
+	}
+
+	async renameFile(file: TFile) {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		const activeFile = view?.file;
+
+		if (!file.parent || !activeFile) {
+			new Notice(`Failed to rename: parent / activeFile is null`);
+			return;
+		}
+
+		const newNameGenerated = await this.generateImageFileName(
+			file,
+			activeFile.name
+		);
+		if (!newNameGenerated) {
+			new Notice(`Failed to generate image name for ${file.name}`);
+			return;
+		}
+		const { name: newName } = await this.deduplicateNewName(
+			newNameGenerated,
+			file
+		);
+		const originName = file.name;
+
+		let linkText = this.app.fileManager.generateMarkdownLink(
+			file,
+			activeFile.path
+		);
+
+		linkText = removeDirectoryPath(linkText);
+
+		const newPath = path.join(file.parent.path, newName);
+		try {
+			await this.app.fileManager.renameFile(file, newPath);
+		} catch (err) {
+			new Notice(`Failed to rename ${newName}: ${err}`);
+			throw err;
+		}
+
+		const newLinkText = this.app.fileManager.generateMarkdownLink(
+			file,
+			activeFile.path
+		);
+
+		const editor = view?.editor;
+		if (!editor) {
+			new Notice(`Failed to rename ${newName}: no active editor`);
+			return;
+		}
+
+		const cursor = editor.getCursor();
+		const line = editor.getLine(cursor.line);
+		const replacedLine = line.replace(linkText, newLinkText);
+
+		editor.transaction({
+			changes: [
+				{
+					from: { ...cursor, ch: 0 },
+					to: { ...cursor, ch: line.length },
+					text: replacedLine,
+				},
+			],
+		});
+
+		new Notice(`Renamed ${originName} to ${newName}`);
+	}
+
+	async deduplicateNewName(newName: string, file: TFile): Promise<NameObj> {
+		// list files in dir
+		const dir = file.parent?.path;
+		if (!dir) {
+			throw new Error("Parent directory is null");
+		}
+		const listed = await this.app.vault.adapter.list(dir);
+
+		// parse newName
+		const newNameExt = path.extname(newName),
+			newNameStem = newName.slice(
+				0,
+				newName.length - newNameExt.length - 1
+			),
+			newNameStemEscaped = escapeRegExp(newNameStem),
+			delimiter = "_",
+			delimiterEscaped = escapeRegExp(delimiter);
+
+		let dupNameRegex;
+
+		dupNameRegex = new RegExp(
+			`^(?<number>\\d+)${delimiterEscaped}(?<name>${newNameStemEscaped})\\.${newNameExt}$`
+		);
+
+		const dupNameNumbers: number[] = [];
+		let isNewNameExist = false;
+		for (let sibling of listed.files) {
+			sibling = path.basename(sibling);
+			if (sibling == newName) {
+				isNewNameExist = true;
+				continue;
 			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+			// match dupNames
+			const m = dupNameRegex.exec(sibling);
+			if (!m) continue;
+			// parse int for m.groups.number
+			if (m.groups) {
+				dupNameNumbers.push(parseInt(m.groups.number));
+			}
+		}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		if (isNewNameExist) {
+			// get max number
+			const newNumber =
+				dupNameNumbers.length > 0 ? Math.max(...dupNameNumbers) + 1 : 1;
+			// change newName
+			newName = `${newNameStem}${delimiter}${newNumber}.${newNameExt}`;
+		}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		return {
+			name: newName,
+			stem: newName.slice(0, newName.length - newNameExt.length - 1),
+			extension: newNameExt,
+		};
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async generateImageFileName(file: TFile, noteName: string) {
+		const dataUrl = await this.getBase64ImageUrl(file);
+		if (!dataUrl) {
+			console.error("Failed to get base64 image url");
+			return null;
+		}
+		try {
+			const response = await openai.chat.completions.create({
+				model: "gpt-4o",
+				messages: [
+					{
+						role: "system",
+						content: `
+    The user has pasted an image into a note. Generate a concise and descriptive file name for this image. Along with the image, you will be given the title of the note as a reference. Try to include the title of note in the image. But the file name should based more on the actual image rather than the title of the note. The generated file name should describe well what is in the image.
+    The file name should be lowercase, use hyphens instead of spaces, and include the appropriate file extension.
+    Give only the generated file name as the output, nothing else.
+        `,
+					},
+					{
+						role: "user",
+						content: [
+							{
+								type: "text",
+								text: `
+          Title of the note: ${noteName}
+          Output:
+          `,
+							},
+							{
+								type: "image_url",
+								image_url: {
+									url: dataUrl,
+								},
+							},
+						],
+					},
+				],
+			});
+
+			return response.choices[0]?.message?.content?.trim() ?? null;
+		} catch (error) {
+			console.error("Error:", error);
+			return null;
+		}
+	}
+
+	async getBase64ImageUrl(file: TFile) {
+		try {
+			const arrayBuffer = await this.app.vault.adapter.readBinary(
+				file.path
+			);
+
+			const base64 = btoa(
+				new Uint8Array(arrayBuffer).reduce(
+					(data, byte) => data + String.fromCharCode(byte),
+					""
+				)
+			);
+
+			return `data:image/jpeg;base64,${base64}`;
+		} catch (error) {
+			console.error("Error reading file:", error);
+			return null;
+		}
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+function removeDirectoryPath(linkText: string) {
+	const match = linkText.match(/\[\[(.*?)\]\]/);
+	if (match && match[1]) {
+		let path = match[1];
+		const fileName = path.split("/").pop();
+		return `![[${fileName}]]`;
 	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+	return linkText;
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+function escapeRegExp(s: string) {
+	return s.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
 
-	constructor(app: App, plugin: MyPlugin) {
+function isPastedImage(file: TAbstractFile): boolean {
+	if (file instanceof TFile) {
+		if (file.name.startsWith(PASTED_IMAGE_PREFIX)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function isMarkdownFile(file: TAbstractFile): boolean {
+	if (file instanceof TFile) {
+		if (file.extension === "md") {
+			return true;
+		}
+	}
+	return false;
+}
+
+const IMAGE_EXTS = ["jpg", "jpeg", "png", "gif", "bmp", "svg"];
+
+function isImage(file: TAbstractFile): boolean {
+	if (file instanceof TFile) {
+		if (IMAGE_EXTS.contains(file.extension.toLowerCase())) {
+			return true;
+		}
+	}
+	return false;
+}
+
+class ImageRenamePluginSettingTab extends PluginSettingTab {
+	plugin: ImageRenamePlugin;
+
+	constructor(app: App, plugin: ImageRenamePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
+}
+
+export interface NameObj {
+	name: string;
+	stem: string;
+	extension: string;
 }
